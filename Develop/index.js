7d736e91af81d05b9b282e52df1d7be7c3c8c135
CambiG1123 <campbellgilliland@gmail.com>
2023-08-28 14:47:30 -0400

    created the init function

--- Develop/index.js
+++ Develop/index.js
@@ -70,7 +70,12 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) =>{
+        console.log("Creating your Professional README please wait")
+        writeToFile("Develop\README.md",generateMarkdown({...responses}))
+    })
+}
 
 // Function call to initialize app
 init();
