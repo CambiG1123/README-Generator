3c41242138f72965e4c44baa32feff4245dfa7a2
CambiG1123 <campbellgilliland@gmail.com>
2023-08-28 14:21:14 -0400

    added the packages needed and installed inquirer

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fS = require('fs')
+const path = require('node:path')
+const generateMarkdown = require("Develop/utils/generateMarkdown.js")
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
